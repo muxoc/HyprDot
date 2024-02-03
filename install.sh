7d27f80c90c94f10aa30064a668c37201f17ce9a
Michael <muxoc.26@gmail.com>
2024-02-03 20:04:45 +0300

    add root_install

--- install.sh
+++ install.sh
@@ -2,10 +2,3 @@
 # https://github.com/muxoc
 
 
-# Check if running as root. If root, scrypt will exit.
-if [[ $EUID -eq 0 ]]; then
-  echo "This scrypt should not be executed as root! Exiting......"
-  exit 1
-fi 
-
-clear
